d65b3862151d279d214ff881d233dcbabaff2d26
Gustav Berggren <gustav.berggren@nav.no>
2023-03-13 11:09:49 +0100

    Opprett datastream private connection med vpc peering
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -13,3 +13,16 @@ resource "google_compute_global_address" "tbd_datastream_vpc_ip_range" {
   network       = google_compute_network.tbd_datastream_private_vpc.id
   prefix_length = 20
 }
+
+// Private connectivity lets you create a peered configuration between your VPC and Datastream’s private network.
+// A single configuration can be used by all streams and connection profiles within a single region.
+resource "google_datastream_private_connection" "tbd_datastream_private_connection" {
+  location              = var.gcp_project["region"]
+  display_name          = "tbd-datastream-private-connection"
+  private_connection_id = "tbd-datastream-private-connection"
+
+  vpc_peering_config {
+    vpc    = google_compute_network.tbd_datastream_private_vpc.id
+    subnet = "10.166.0.0/29"
+  }
+}
