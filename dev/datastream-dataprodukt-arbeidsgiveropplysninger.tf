493477006b8919aea48b13047c675ffe32e6cf2c
Gustav Berggren <gustav.berggren@nav.no>
2023-03-14 15:13:33 +0100

    Opprett datastream for arbeidsgiveropplysninger databasen
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- dev/datastream-dataprodukt-arbeidsgiveropplysninger.tf
+++ dev/datastream-dataprodukt-arbeidsgiveropplysninger.tf
@@ -33,3 +33,52 @@ resource "google_datastream_connection_profile" "arbeidsgiveropplysninger_postgr
     private_connection = google_datastream_private_connection.tbd_datastream_private_connection.id
   }
 }
+
+resource "google_datastream_stream" "arbeidsgiveropplysninger_datastream" {
+  stream_id     = "arbeidsgiveropplysninger-datastream"
+  display_name  = "arbeidsgiveropplysninger-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.arbeidsgiveropplysninger_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "dataprodukter_arbeidsgiveropplysninger_publication"
+      replication_slot              = "dataprodukter_arbeidsgiveropplysninger_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "900s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.arbeidsgiveropplysninger_dataset.dataset_id}"
+      }
+    }
+  }
+}
