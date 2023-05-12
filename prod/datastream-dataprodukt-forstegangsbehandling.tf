6c6b0d2e8c5731ba7233b136d7a7a224dce2cd1c
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-05-12 10:20:25 +0200

    Legger til datastream for forstegangsbehandling
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- prod/datastream-dataprodukt-forstegangsbehandling.tf
+++ prod/datastream-dataprodukt-forstegangsbehandling.tf
@@ -33,3 +33,61 @@ resource "google_datastream_connection_profile" "forstegangsbehandling_postgresq
     private_connection = google_datastream_private_connection.tbd_datastream_private_connection.id
   }
 }
+
+resource "google_datastream_stream" "forstegangsbehandling_datastream" {
+  stream_id     = "forstegangsbehandling-datastream"
+  display_name  = "forstegangsbehandling-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.forstegangsbehandling_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "dataprodukter_forstegangsbehandling_publication"
+      replication_slot              = "dataprodukter_forstegangsbehandling_replication"
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "soknad"
+            postgresql_columns {
+              column = "hendelse_id"
+            }
+            postgresql_columns {
+              column = "soknad_id"
+            }
+            postgresql_columns {
+              column = "sykmelding_id"
+            }
+            postgresql_columns {
+              column = "opprettet"
+            }
+            postgresql_columns {
+              column = "forstegangsbehandling"
+            }
+          }
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "900s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.forstegangsbehandling_dataset.dataset_id}"
+      }
+    }
+  }
+}
