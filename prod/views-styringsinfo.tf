310c57dec3060464a6eb84537f60be33c630d5fb
Øyvind Hagen <oyvind.hagen@nav.no>
2023-07-13 10:44:09 +0200

    🔧 Legger til view basert på vedtak_fattet (i prod)
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>

--- prod/views-styringsinfo.tf
+++ prod/views-styringsinfo.tf
@@ -53,7 +53,6 @@ module "styringsinfo_sendt_soknad_view" {
         type        = "TIMESTAMP"
         description = "Tidspunktet bruker sendte søknaden til arbeidsgiver eller NAV første gang."
       }
-
     ]
   )
   view_query = <<EOF
@@ -65,5 +64,50 @@ SELECT
     sendt AS soknad_mottatt
 FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.spre_styringsinfo_dataset.dataset_id}.public_sendt_soknad`
 EOF
+}
 
+module "styringsinfo_vedtak_fattet_view" {
+  source              = "../modules/google-bigquery-view"
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.styringsinfo_dataset.dataset_id
+  view_id             = "styringsinfo_vedtak_fattet_view"
+  view_description    = "Basert på vedtak_fattet-hendelser på tbd.rapid.v1."
+  view_schema = jsonencode(
+    [
+      {
+        name        = "syntetisk_id"
+        type        = "STRING"
+        description = "Syntetisk id tildelt ved lagring av hendelser i spre-styringsinfo."
+      },
+      {
+        name        = "hendelse_id"
+        type        = "STRING"
+        description = "Intern id laget når hendelsen legges på tbd.rapid.v1"
+      },
+      {
+        name        = "vedtaksperiode_id"
+        type        = "STRING"
+        description = "Vedtaksperioden som hendelsen refererer til."
+      },
+      {
+        name        = "utbetaling_id"
+        type        = "STRING"
+        description = "Eventuell utbetaling som vedtaket bidrar til."
+      },
+      {
+        name        = "vedtak_fattet"
+        type        = "TIMESTAMP"
+        description = "Tidspunktet vedtaket ble fattet."
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT
+    id AS syntetisk_id,
+    hendelse_id,
+    JSON_EXTRACT_SCALAR(melding, '$.vedtaksperiodeId') AS vedtaksperiode_id,
+    JSON_EXTRACT_SCALAR(melding, '$.utbetalingId') AS utbetaling_id,
+    vedtak_fattet_tidspunkt AS vedtak_fattet
+FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.spre_styringsinfo_dataset.dataset_id}.public_vedtak_fattet`
+EOF
 }
