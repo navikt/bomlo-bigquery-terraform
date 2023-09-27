3ccb98843eb32d5fa5634a6163188de95de40869
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-09-27 15:48:25 +0200

    Revert "Slett datastream-spre-styringsinfo i prod"
    This reverts commit 76e5ef215006b0cbc06f0f10dfa84c7c15f5a170.

--- prod/datastream-spre-styringsinfo.tf
+++ prod/datastream-spre-styringsinfo.tf
@@ -44,3 +44,73 @@ resource "google_datastream_connection_profile" "spre_styringsinfo_postgresql_co
   }
 }
 
+resource "google_datastream_stream" "spre_styringsinfo_datastream" {
+  stream_id     = "spre-styringsinfo-datastream"
+  display_name  = "spre-styringsinfo-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.spre_styringsinfo_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "spre_styringsinfo_publication"
+      replication_slot              = "spre_styringsinfo_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+
+          postgresql_tables {
+            table = "sendt_soknad"
+            postgresql_columns {
+              column = "patch_level"
+            }
+          }
+
+          postgresql_tables {
+            table = "vedtak_fattet"
+            postgresql_columns {
+              column = "patch_level"
+            }
+          }
+
+          postgresql_tables {
+            table = "vedtak_forkastet"
+            postgresql_columns {
+              column = "patch_level"
+            }
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "3600s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.spre_styringsinfo_dataset.dataset_id}"
+      }
+    }
+  }
+}
+
