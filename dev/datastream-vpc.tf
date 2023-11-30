7d1f42367b96063179b5dbedf6c7a2d87d1b6cd7
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-11-30 11:57:59 +0100

    Bruk e2-small for Cloud SQL Proxy VM i DEV

--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -77,7 +77,7 @@ module "cloud_sql_auth_proxy_container_datastream" {
 // Create a VM used to host the Cloud SQL reverse proxy.
 resource "google_compute_instance" "tbd_datastream_cloud_sql_proxy_vm" {
   name         = "tbd-datastream-cloud-sql-proxy-vm"
-  machine_type = "e2-micro"
+  machine_type = "e2-small"
   project      = var.gcp_project["project"]
   zone         = var.gcp_project["zone"]
 
