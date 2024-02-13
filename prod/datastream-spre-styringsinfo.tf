45e72fe17dfb271b2f018348b4c7de8cb4bb9c69
Hege Haavaldsen <hege.haavaldsen@nav.no>
2024-02-13 10:26:01 +0100

    Revert "Fjerner tabell som ikke finnes ennå"
    This reverts commit 1b50bcd7f8b3905330a6947140a2fe5bb9e5e770.

--- prod/datastream-spre-styringsinfo.tf
+++ prod/datastream-spre-styringsinfo.tf
@@ -69,6 +69,11 @@ resource "google_datastream_stream" "spre_styringsinfo_datastream" {
           postgresql_tables {
             table = "flyway_schema_history"
           }
+
+          postgresql_tables {
+            table = "hendelse"
+          }
+
           postgresql_tables {
             table = "sendt_soknad"
             postgresql_columns {
