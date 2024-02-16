6f9f640357a3ea627e5a69e4bdbd849d54cf8c71
Eirik <eirik.kakepepper.org>
2024-02-16 13:56:56 +0100

    Gir en person i team sak tilgang til et view
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- dev/secrets.tf
+++ dev/secrets.tf
@@ -7,3 +7,12 @@ locals {
     data.google_secret_manager_secret_version.spre_styringsinfo_datastream_user_secret.secret_data
   )
 }
+
+// Epost-adresse vi gir tilgang til å lese data
+data "google_secret_manager_secret_version" "team_sak_epost_secret" {
+  secret = "team_sak_epost_secret"
+}
+
+locals {
+  team_sak_epost = data.google_secret_manager_secret_version.team_sak_epost_secret.secret_data
+}
--- dev/views-team-sak.tf
+++ dev/views-team-sak.tf
@@ -179,3 +179,12 @@ ON
   order by sekvensnummer
 EOF
 }
+
+
+resource "google_bigquery_table_iam_binding" "behandlingshendelse_view_iam_binding" {
+  project    = var.gcp_project.project
+  dataset_id = google_bigquery_dataset.saksbehandlingsstatistikk_til_team_sak_dataset.dataset_id
+  table_id   = module.saksbehandlingsstatistikk_til_team_sak_view.bigquery_view_id
+  role       = "roles/bigquery.dataViewer"
+  members    = ["user:${local.team_sak_epost}"]
+}
