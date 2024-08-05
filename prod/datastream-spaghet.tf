b50a2889a7213760499a3374f5683011b9931049
Eirik Gallefoss <eirik.gallefoss@nav.no>
2024-08-05 13:08:14 +0200

    ✨ Streamer soknad fra spaghet til bigquery
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- prod/datastream-spaghet.tf
+++ prod/datastream-spaghet.tf
@@ -87,6 +87,10 @@ resource "google_datastream_stream" "spaghet_datastream" {
           postgresql_tables {
             table = "varsel"
           }
+
+          postgresql_tables {
+            table = "soknad"
+          }
         }
       }
     }
