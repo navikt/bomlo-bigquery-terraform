c8b3c0b052fc1a65b548f9967b060e923a528e60
Sivert Lunsæter <sfl@computas.com>
2025-03-03 13:52:31 +0100

    Legg til bigquery connection profiles i terraform. Denne push og apply vil også starte datastreamene på nytt, da postgres brukerne har fått nytt passord

---
+++ prod/bigquery-connections.tf
@@ -0,0 +1,67 @@
+data "google_sql_database_instance" "spesialist-instance-db" {
+    name = "spesialist"
+}
+
+resource "google_bigquery_connection" "spesialist-bigquery-connection" {
+    connection_id = "spesialist"
+    location      = "europe-north1"
+    friendly_name = "spesialist"
+    description   = "Kobling til spesialist postgres basen fra BigQuery"
+    cloud_sql {
+        instance_id = data.google_sql_database_instance.spesialist-instance-db.connection_name
+        database    = "spesialist"
+        type        = "POSTGRES"
+        credential {
+          username = local.spesialist_bigquery_connection_user.username
+          password = local.spesialist_bigquery_connection_user.password
+        }
+    }
+}
+
+resource "google_bigquery_connection" "spaghet-bigquery-connection" {
+    connection_id = "spaghet"
+    location      = "europe-north1"
+    friendly_name = "spaghet"
+    description   = "Kobling til spaghet postgres basen fra BigQuery"
+    cloud_sql {
+        instance_id = data.google_sql_database_instance.spaghet_db.connection_name
+        database    = "spaghet"
+        type        = "POSTGRES"
+        credential {
+          username = local.spaghet_bigquery_connection_user.username
+          password = local.spaghet_bigquery_connection_user.password
+        }
+    }
+}
+
+resource "google_bigquery_connection" "annulleringer-bigquery-connection" {
+    connection_id = "dataprodukt-annulleringer"
+    location      = "europe-north1"
+    friendly_name = "Dataprodukt annulleringer"
+    description   = "Kobling til dataproduktet annulleringer sin postgres base fra BigQuery"
+    cloud_sql {
+        instance_id = data.google_sql_database_instance.annulleringer_db.connection_name
+        database    = "annulleringer"
+        type        = "POSTGRES"
+        credential {
+          username = local.annulleringer_bigquery_connection_user.username
+          password = local.annulleringer_bigquery_connection_user.password
+        }
+    }
+}
+
+resource "google_bigquery_connection" "forstegangsbehandling-bigquery-connection" {
+    connection_id = "dataprodukt-forstegangsbehandlinger"
+    location      = "europe-north1"
+    friendly_name = "Dataprodukt forstegangsbehandling"
+    description   = "Kobling til dataproduktet førstegangsbehandling basen fra BigQuery"
+    cloud_sql {
+        instance_id = data.google_sql_database_instance.dataprodukt_forstegangsbehandling_db.connection_name
+        database    = "forstegangsbehandling"
+        type        = "POSTGRES"
+        credential {
+          username = local.forstegangsbehandlinger_bigquery_connection_user.username
+          password = local.forstegangsbehandlinger_bigquery_connection_user.password
+        }
+    }
+}
--- prod/secrets.tf
+++ prod/secrets.tf
@@ -18,6 +18,22 @@ data "google_secret_manager_secret_version" "annulleringer_datastream_user_secre
   secret = "annulleringer_datastream_user_secret"
 }
 
+data "google_secret_manager_secret_version" "spaghet_bigquery_connection_user_secret" {
+  secret = "spaghet_bigquery_connection_user_secret"
+}
+
+data "google_secret_manager_secret_version" "annulleringer_bigquery_connection_user_secret" {
+  secret = "annulleringer_bigquery_connection_user_secret"
+}
+
+data "google_secret_manager_secret_version" "forstegangsbehandlinger_bigquery_connection_user_secret" {
+  secret = "forstegangsbehandlinger_bigquery_connection_user_secret"
+}
+
+data "google_secret_manager_secret_version" "spesialist_bigquery_connection_user_secret" {
+  secret = "spesialist_bigquery_connection_user_secret"
+}
+
 # Locals for å decode secrets fra JSON format
 locals {
   dataprodukt_arbeidsgiveropplysninger_db_credentials = jsondecode(
@@ -38,5 +54,21 @@ locals {
 
   dataprodukt_annulleringer_db_credentials = jsondecode(
     data.google_secret_manager_secret_version.annulleringer_datastream_user_secret.secret_data
+  )
+
+  spaghet_bigquery_connection_user = jsondecode(
+    data.google_secret_manager_secret_version.spaghet_bigquery_connection_user_secret.secret_data
+  )
+
+  annulleringer_bigquery_connection_user = jsondecode(
+    data.google_secret_manager_secret_version.annulleringer_bigquery_connection_user_secret.secret_data
+  )
+
+  forstegangsbehandlinger_bigquery_connection_user = jsondecode(
+    data.google_secret_manager_secret_version.forstegangsbehandlinger_bigquery_connection_user_secret.secret_data
+  )
+
+  spesialist_bigquery_connection_user = jsondecode(
+    data.google_secret_manager_secret_version.spesialist_bigquery_connection_user_secret.secret_data
   )
 }
