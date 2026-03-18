e1c297e61e9b19164c4d70f79ac063d3bba60b49
Sivert Lunsæter <sfl@computas.com>
2026-03-18 13:29:57 +0100

    Revert "Gi logg sink'en sin service konto dataEditor i datasettet den skal skrive logger til"
    This reverts commit 1e479666b500394e278be7f63ca3063b92b6456b.

--- prod/log-sink.tf
+++ prod/log-sink.tf
@@ -24,14 +24,4 @@ resource "google_logging_project_sink" "overstyr_tidslinje_logs" {
   description = "En logg sink som sender logger Spleis skriver når den får en overstyr_tidslinje melding til BQ"
   destination = "bigquery.googleapis.com/${google_bigquery_dataset.log_sink.id}"
   filter      = "resource.type=\"k8s_container\" AND resource.labels.container_name=\"spleis\" AND jsonPayload.message: \"gjenkjente OverstyrTidslinjeMessage\""
-
-  unique_writer_identity = true
-}
-
-
-// Gi SA'en til log sinken tilgang til å skrive til BigQuery
-resource "google_bigquery_dataset_iam_member" "log_sink_sa_writer" {
-  dataset_id = google_bigquery_dataset.log_sink.dataset_id
-  role       = "roles/bigquery.dataEditor"
-  member     = google_logging_project_sink.overstyr_tidslinje_logs.writer_identity
 }
