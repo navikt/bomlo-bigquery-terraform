7c30cae2306ffc971945353ffe94b351ebf98fb6
Sivert Lunsæter <sfl@computas.com>
2026-03-18 13:29:57 +0100

    Revert "Fiks formatering"
    This reverts commit 2c7b3b7d7bfde3ba5653eb778ca17bf593ec2deb.

--- prod/log-sink.tf
+++ prod/log-sink.tf
@@ -5,23 +5,23 @@ resource "google_bigquery_dataset" "log_sink" {
   location   = var.gcp_project["region"]
   project    = var.gcp_project["project"]
   access {
-    role          = "OWNER"
-    special_group = "projectOwners"
+      role          = "OWNER"
+      special_group = "projectOwners"
   }
   access {
-    role          = "READER"
-    special_group = "projectReaders"
+      role          = "READER"
+      special_group = "projectReaders"
   }
   access {
-    role          = "WRITER"
-    special_group = "projectWriters"
+      role          = "WRITER"
+      special_group = "projectWriters"
   }
 }
 
 // Log sink
 resource "google_logging_project_sink" "overstyr_tidslinje_logs" {
-  name        = "overstyr_tidslinje_logs"
+  name = "overstyr_tidslinje_logs"
   description = "En logg sink som sender logger Spleis skriver når den får en overstyr_tidslinje melding til BQ"
   destination = "bigquery.googleapis.com/${google_bigquery_dataset.log_sink.id}"
-  filter      = "resource.type=\"k8s_container\" AND resource.labels.container_name=\"spleis\" AND jsonPayload.message: \"gjenkjente OverstyrTidslinjeMessage\""
+  filter = "resource.type=\"k8s_container\" AND resource.labels.container_name=\"spleis\" AND jsonPayload.message: \"gjenkjente OverstyrTidslinjeMessage\""
 }
